1452edc356b6f0181fe8e971869b59f4ce395b27
Raul Ciobanu <rauliathos@gmail.com>
2020-11-21 13:35:08 +0000

    whan number guessed change background to green and enlarge encreased the width of number

--- script.js
+++ script.js
@@ -20,13 +20,16 @@ document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function() {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
-
+  // When there is no imput
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No number!";
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
-    score++;
-    document.querySelector(".score").textContent = score;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
@@ -36,6 +39,7 @@ document.querySelector(".check").addEventListener("click", function() {
       document.querySelector(".message").textContent = "💥 You lost the game!";
       document.querySelector(".score").textContent = 0;
     }
+    // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!";
