f92b81056924d085549546445577e7c6370adc1c
Raul Ciobanu <rauliathos@gmail.com>
2020-11-21 13:15:57 +0000

    created secretNumber as a random number, added eventListener on click and created game logic

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-'use strict';
+"use strict";
+/*
+console.log(document.querySelector(".message").textContent);
 
-// console.log(document.querySelector('.message').textContent);
+document.querySelector(".message").textContent = "🎉 Correct Number!";
+console.log(document.querySelector(".message").textContent);
 
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function() {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔️ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+    score++;
+    document.querySelector(".score").textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--; /* score = score-1;*/
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low!";
+      score--; /* score = score-1;*/
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
